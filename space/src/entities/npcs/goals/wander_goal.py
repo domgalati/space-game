3e77dd3359bf2c113c9e640a283000fdff008e8e
Dom <domenickgalati@gmail.com>
2024-01-18 21:58:01 -0500

    properly wandering npcs

---
+++ space/src/entities/npcs/goals/wander_goal.py
@@ -0,0 +1,39 @@
+import random
+from .base_goal import BaseGoal
+
+# class WanderGoal(BaseGoal):
+#     def __init__(self, npc):
+#         super().__init__(npc)
+
+
+#     def update(self):
+#         # Randomly move the NPC up, down, left, or right
+#         direction = random.choice(['up', 'down', 'left', 'right'])
+#         if direction == 'up':
+#             self.npc.position = (self.npc.position[0], self.npc.position[1] - 1)
+#         elif direction == 'down':
+#             self.npc.position = (self.npc.position[0], self.npc.position[1] + 1)
+#         elif direction == 'left':
+#             self.npc.position = (self.npc.position[0] - 1, self.npc.position[1])
+#         elif direction == 'right':
+#             self.npc.position = (self.npc.position[0] + 1, self.npc.position[1])
+
+class WanderGoal(BaseGoal):
+    def __init__(self, npc):
+        super().__init__(npc)
+
+    def update(self):
+        walkable_tiles = self.npc.npc_manager.get_walkable_tiles()
+        potential_positions = self.get_adjacent_walkable_positions(walkable_tiles)
+        if potential_positions:
+            self.npc.position = random.choice(potential_positions)
+
+    def get_adjacent_walkable_positions(self, walkable_tiles):
+        x, y = self.npc.position
+        adjacent_positions = [
+            (x, y - 1),  # Up
+            (x, y + 1),  # Down
+            (x - 1, y),  # Left
+            (x + 1, y)   # Right
+        ]
+        return [pos for pos in adjacent_positions if pos in walkable_tiles]
